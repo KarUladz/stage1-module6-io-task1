3ac280b6cf14a22c6c90408154a79004cbad26db
KarUladz <u.karnialiuk@gmail.com>
2025-07-30 20:42:38 +0300

    refactor: I/O task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,36 +1,47 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
         String name = null;
         int age = 0;
         String email = null;
         Long phone = null;
 
-        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                final String lineStringTrimmed = line.substring(line.indexOf(":") + 1).trim();
-                if (line.startsWith("Name:")) {
-                    name = lineStringTrimmed;
-                } else if (line.startsWith("Age:")) {
-                    age = Integer.parseInt(lineStringTrimmed);
-                } else if (line.startsWith("Email:")) {
-                    email = lineStringTrimmed;
-                } else if (line.startsWith("Phone:")) {
-                    phone = Long.parseLong(lineStringTrimmed);
+        try (InputStream inputStream = new FileInputStream(file)) {
+            int c = inputStream.read();
+            StringBuilder sb = new StringBuilder();
+            while (c != -1) {
+                sb.append((char) c);
+                if ((char) c == '\n') {
+                    final String titleLineTrimmed = sb.substring(0, sb.indexOf(":")).trim();
+                    final String infoLineTrimmed = sb.substring(sb.indexOf(":") + 1).trim();
+                    switch (titleLineTrimmed) {
+                        case "Name":
+                            name = infoLineTrimmed;
+                            break;
+                        case "Age":
+                            age = Integer.parseInt(infoLineTrimmed);
+                            break;
+                        case "Email":
+                            email = infoLineTrimmed;
+                            break;
+                        case "Phone":
+                            phone = Long.parseLong(infoLineTrimmed);
+                            break;
+                        default:
+                            break;
+                    }
+                    sb.setLength(0);
                 }
+                c = inputStream.read();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException("Failed to read profile file", e);
         }
-
         return new Profile(name, age, email, phone);
     }
 }
